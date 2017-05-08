813bd26858b6c1277e313d9fe02c27fb5d189133
wanghui <wh_linux@126.com>
2017-05-08 15:21:22 +0800

    Adjust setup & add tox.ini

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-""" """
 from __future__ import print_function, division, unicode_literals
+
+import os
 
 from setuptools import setup
 
+os.environ["SKIP_GENERATE_AUTHORS"] = "1"
+os.environ["SKIP_WRITE_GIT_CHANGELOG"] = "1"
+
 setup(
     setup_requires=["pbr>=1.9", "setuptools>=17.1", "pytest-runner"],
-    tests_require=["python-dotenv", 'pytest'],
+    tests_require=["python-dotenv", 'pytest', 'coverage', 'pytest-cov'],
     pbr=True
 )
